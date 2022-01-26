9b048e0074061eeeec0feb1039f8ac27522bc8cd
Medha <tyagi.25@gmail.com>
2022-01-26 20:42:51 +0000

    added load balancer

--- ec2-config/instance.tf
+++ ec2-config/instance.tf
@@ -1,61 +1,10 @@
-# Filter the aws ami
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-# Define the Security Group
-resource "aws_security_group" "public_ec2_01_sg" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = data.terraform_remote_state.network-config.outputs.vpc_id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
-}
-
 # EC2 Instance
-resource "aws_instance" "public_ec2_01" {
+resource "aws_instance" "private_ec2_01" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  subnet_id                   = data.terraform_remote_state.network-config.outputs.public_subnets[0].id
-  security_groups             = [aws_security_group.public_ec2_01_sg.id]
+  associate_public_ip_address = false
+  subnet_id                   = data.terraform_remote_state.network-config.outputs.private_subnets[0].id
+  security_groups             = [aws_security_group.private_ec2_01_sg.id]
   key_name                    = "public_ec2_01_kp" # this is manually created, delete this key
 
   user_data = <<-EOF
@@ -65,6 +14,6 @@ resource "aws_instance" "public_ec2_01" {
 
   EOF
   tags = {
-    Name = "Public-EC2"
+    Name = "Private-EC2"
   }
 }
